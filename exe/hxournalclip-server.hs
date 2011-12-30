a4a64c03fd79f104a85e8affa8595cb897f0604f
ianwookim <ianwookim@gmail.com>
2011-12-30 22:49:50 +0000

    add some test case

--- exe/hxournalclip-server.hs
+++ exe/hxournalclip-server.hs
@@ -8,8 +8,19 @@ import Yesod
 import qualified Data.Map as M
 import Data.Acid 
 
+import Data.Xournal.Simple 
+import Data.Strict.Tuple
+import Data.UUID.V5 
+import Application.Hxournal.NetworkClipboard.Type 
+
+teststroke = [Stroke "test" "test" 1.1 [ 0.0 :!: 1.0 ] ]
+
+teststrokeinfo = HxournalclipInfo namespaceURL teststroke
+
+
+
 main :: IO ()
 main = do 
   putStrLn "hxournalclip-server"
-  acid <- openLocalState M.empty 
+  acid <- openLocalState (M.insert namespaceURL teststrokeinfo M.empty )
   warpDebug 7800 (HxournalclipServer acid)
